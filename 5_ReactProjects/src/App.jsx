fe3cbb44c8e89e1d0b1168ed6438e737f8f5b135
Mangesh Rajguru <msrajguru7@gmail.com>
2026-03-05 14:45:10 +0530

    Added react project as a single file

---
+++ 5_ReactProjects/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <h1>Mangesh Rajguru is Here...</h1>
+    <h2>He is best programmer in the World...</h2>
+    </>
+  )
+}
+
+export default App
